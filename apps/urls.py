11705ace4781f5a100a23374d6f4369b9f788ba3
adilraja11 <adilraja19@students.undip.ac.id>
2025-04-09 00:14:00 +0700

    feat: add create event, login, register, and logout logic

--- apps/urls.py
+++ apps/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import EventCreateView, EventDetailView, EventListView
+from .views import EventCreateView, EventDetailView, EventListView, RegisterView, LoginView, LogoutView
 
 urlpatterns = [
     path("", EventListView.as_view(), name="event_list"),
     path("<str:id>", EventDetailView.as_view(), name="event_detail"),
     path("create/", EventCreateView.as_view(), name="event_create"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,10 +1,58 @@
 from django.shortcuts import redirect, render
 from django.views.generic import DetailView, ListView, View
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 
 from .models import Event
+from .forms import UserRegistrationForm
 
 
 # Create your views here.
+class RegisterView(View):
+    def get(self, request):
+        form = UserRegistrationForm()
+        return render(request, "register.html", {"form": form})
+
+    def post(self, request):
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registration successful.")
+            return redirect("login")
+        else:
+            messages.error(request, "Registration failed. Please try again.")
+            return render(request, "register.html", {"form": form})
+
+class LoginView(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, "login.html", {"form": form})
+
+    def post(self, request):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect("event_list")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+
+        return render(request, "login.html", {"form": form})
+    
+class LogoutView(View):
+    def post(self, request):
+        logout(request)
+        messages.success(request, "Logged out successfully.")
+        return redirect("event_list")
+
 class EventListView(ListView):
     model = Event
     template_name = "event_list.html"
@@ -21,7 +69,11 @@ class EventDetailView(DetailView):
 
 class EventCreateView(View):
     def get(self, request):
-        return render(request, "event_create.html")
+        context = {
+            'LOCATION_CHOICES': Event.LOCATION_CHOICES,
+        }
+
+        return render(request, "event_create.html", context)
 
     def post(self, request):
         name = request.POST.get("name")
