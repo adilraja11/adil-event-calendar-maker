6dbb9de00158692702633fa88aae7e362d9f159c
adilraja11 <marsinf@EDTS022184.>
2025-04-08 15:37:24 +0700

    feat: add list event

---
+++ apps/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import EventListView, EventDetailView, EventCreateView
+
+urlpatterns = [
+    path('', EventListView.as_view(), name='event_list'),
+    path('<int:pk>', EventDetailView.as_view(), name='event_detail'),
+    path('create/', EventCreateView.as_view(), name='event_create'),
+]
--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import ListView, View
+from django.shortcuts import redirect
+
+from .models import Event
 
 # Create your views here.
+class EventListView(ListView):
+    model = Event
+    template_name = 'event_list.html'
+    context_object_name = 'events'
+
+class EventDetailView(View):
+    model = Event
+    template_name = 'event_detail.html'
+    context_object_name = 'events'
+
+class EventCreateView(View):
+    def get(self, request):
+        return render(request, 'event_create.html')
+    def post(self, request):
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        date = request.POST.get('date')
+        location = request.POST.get('location')
+        created_by = request.user
+        Event.objects.create(
+            name=name,
+            description=description,
+            date=date,
+            location=location,
+            created_by=created_by
+        )
+        
+        return redirect('event_list')
