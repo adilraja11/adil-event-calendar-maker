a18579ebb23ca2c43db827b8adfd13ecfc5c75c8
adilraja11 <adilraja19@students.undip.ac.id>
2025-04-11 06:44:41 +0700

    fix: change urls & add allowed hosts

--- apps/urls.py
+++ apps/urls.py
@@ -5,7 +5,7 @@ from .views import (EventCreateView, EventDetailView, EventListView, LoginView,
 
 urlpatterns = [
     path("", EventListView.as_view(), name="event_list"),
-    path("<str:id>", EventDetailView.as_view(), name="event_detail"),
+    path("event/<str:id>", EventDetailView.as_view(), name="event_detail"),
     path("create/", EventCreateView.as_view(), name="event_create"),
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
